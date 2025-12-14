a7cd1023c6134630fe4bee9896389b6440bf938c
Oscasavia <oscasavia@gmail.com>
2025-12-13 23:48:26 -0600

    trying to fix profile pic

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -153,7 +153,7 @@ const Resume = () => {
               highlighting the skills and growth that have shaped my career.
             </p><br /><br />
             <a
-              href="/assets/docs/myResumeOscasavia.pdf"
+              href="/src/assets/myResumeOscasavia.pdf"
               download="Oscasavia_Resume.pdf"
               className="btn-primary inline-flex"
             >
